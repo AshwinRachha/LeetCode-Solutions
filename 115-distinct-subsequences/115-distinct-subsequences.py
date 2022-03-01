8a525c245e41316613f71bf9eb22f9ea55f3bc2c
Ashwin Rachha <43395687+AshwinRachha@users.noreply.github.com>
2022-03-01 12:58:08 -0500

    Time: 1528 ms (9.13%), Space: 156.9 MB (18.71%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.py
+++ 115-distinct-subsequences/115-distinct-subsequences.py
@@ -9,9 +9,11 @@ class Solution:
                 return 1
             if i >= m:
                 return 0
-            count = backtrack(i+1, j)
+            count = 0
             if s[i] == t[j]:
-                count += backtrack(i+1, j+1)
+                count += backtrack(i+1, j+1) +  backtrack(i+1, j) 
+            else:
+                count = backtrack(i+1, j)
             return count
         
         return backtrack(0, 0)
