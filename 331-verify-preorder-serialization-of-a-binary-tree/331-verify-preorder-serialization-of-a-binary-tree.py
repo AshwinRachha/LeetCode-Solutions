76adec44420628fddef5d82de05d26b304daed25
Ashwin Rachha <43395687+AshwinRachha@users.noreply.github.com>
2022-03-03 12:25:00 -0500

    Time: 52 ms (45.06%), Space: 14 MB (67.24%) - LeetHub

--- 331-verify-preorder-serialization-of-a-binary-tree/331-verify-preorder-serialization-of-a-binary-tree.py
+++ 331-verify-preorder-serialization-of-a-binary-tree/331-verify-preorder-serialization-of-a-binary-tree.py
@@ -8,6 +8,6 @@ class Solution:
             while len(stack) > 2 and stack[-3] != '#' and stack[-2] == '#' and stack[-1] =='#':
                 stack.pop(); stack.pop(); stack.pop();
                 stack.append('#')
-            print(stack)
+            #print(stack)
         return len(stack) == 1 and stack[0] == '#'
         
