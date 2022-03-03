3e26e01efe1c83d85274ceb9ad8117fe6d674e4b
Ashwin Rachha <43395687+AshwinRachha@users.noreply.github.com>
2022-03-03 14:42:39 -0500

    Time: 40 ms (85.97%), Space: 14 MB (47.52%) - LeetHub

---
+++ 43-multiply-strings/43-multiply-strings.py
@@ -0,0 +1,23 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        
+        def to_int(string):
+            result = 0
+            for c in string:
+                result = 10*result + ( ord(c) - ord('1') + 1 )
+            return result
+        
+        def to_string(integer):
+            result = ''
+            if integer == 0:
+                return '0'
+            while integer>0:
+                remainder = integer % 10
+                integer //= 10
+                result += chr(ord('1') + remainder - 1)
+            return result[::-1]
+        
+        result = to_int(num1) * to_int(num2)
+        
+        return to_string(result)
+        
