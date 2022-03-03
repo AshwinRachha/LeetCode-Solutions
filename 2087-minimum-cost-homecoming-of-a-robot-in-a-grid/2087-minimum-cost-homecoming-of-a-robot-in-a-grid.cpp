ca81283e49170ef3a249d8b0cc8930db1d5a0555
Ashwin Rachha <43395687+AshwinRachha@users.noreply.github.com>
2022-03-03 12:08:21 -0500

    Time: 243 ms (62.55%), Space: 149.9 MB (25.23%) - LeetHub

--- 2087-minimum-cost-homecoming-of-a-robot-in-a-grid/2087-minimum-cost-homecoming-of-a-robot-in-a-grid.cpp
+++ 2087-minimum-cost-homecoming-of-a-robot-in-a-grid/2087-minimum-cost-homecoming-of-a-robot-in-a-grid.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
-    int minCost(vector<int>& startPos, vector<int>& homePos, vector<int>& rowCosts, vector<int>& colCosts) 
-    {
+    int minCost(vector<int>& startPos, vector<int>& homePos, vector<int>& rowCosts, vector<int>& colCosts) {
         int ans = 0;
-        
-        for(int i = homePos[0]; i != startPos[0]; i += i > startPos[0] ? -1 : 1)
-        {
+        for(int i = homePos[0]; i != startPos[0]; i += i > startPos[0] ? -1 : 1)  // move row-wise from end to start
             ans += rowCosts[i];
-        }
-        
-        for(int j = homePos[1]; j != startPos[1]; j += j > startPos[1] ? -1 : 1)
-        {
-            ans += colCosts[j];
-        }
+        for(int i = homePos[1]; i != startPos[1]; i += i > startPos[1] ? -1 : 1)  // move col-wise from end to start
+            ans += colCosts[i];
         return ans;
     }
 };
