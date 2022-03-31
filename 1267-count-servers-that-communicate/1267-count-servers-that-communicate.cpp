a97aea7be02cfb73dfb2a5eeed2852fed6c8385a
Ashwin Rachha <43395687+AshwinRachha@users.noreply.github.com>
2022-03-31 07:58:48 -0400

    Time: 88 ms (41.47%), Space: 22.4 MB (32.94%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.cpp
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.cpp
@@ -14,18 +14,20 @@ public:
         }
         
     }
-    int countServers(vector<vector<int>>& grid) {
+    int countServers(vector<vector<int>>& grid) 
+    {
         int ans=0;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j]==1){
+        for(int r=0;r<grid.size();r++){
+            for(int c=0;c<grid[0].size();c++)
+            {
+                if(grid[r][c]==1)
+                {
                     int count=0;
-                    dfs(grid,i,j,count);
-                    if(count>1)ans+=count;
+                    dfs(grid,r,c,count);
+                    if(count>1) ans+=count;
                 }
             }
         }
         return ans;
-        
     }
 };
