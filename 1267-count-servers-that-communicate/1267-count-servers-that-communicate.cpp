bd47e7f1d6701ed674e93e92bec6048eed2118e2
Ashwin Rachha <43395687+AshwinRachha@users.noreply.github.com>
2022-03-31 07:59:12 -0400

    Time: 121 ms (18.56%), Space: 22.4 MB (57.86%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.cpp
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     //dfs travel along row and column and just turn 1 into 0
-    void dfs(vector<vector<int>>& grid, int i ,int j, int &count){
-        if(grid[i][j]==1){
+    void dfs(vector<vector<int>>& grid, int r ,int c, int& count)
+    {
+        if(grid[r][c] == 1)
+        {
             count++;
-            grid[i][j]=0;
-            for(int c=0;c<grid[0].size();c++){
-                if(grid[i][c]==1)dfs(grid,i,c,count);
+            grid[r][c] = 0;
+            for(int i = 0; i < grid.size(); i++)
+            {
+                if(grid[i][c] == 1)
+                    dfs(grid, i, c, count);
             }
-            for(int r=0;r<grid.size();r++){
-                if(grid[r][j]==1)dfs(grid,r,j,count);
+            for(int j = 0; j < grid[0].size(); j++)
+            {
+                if(grid[r][j] == 1)
+                    dfs(grid, r, j, count);
             }
         }
-        
     }
     int countServers(vector<vector<int>>& grid) 
     {
