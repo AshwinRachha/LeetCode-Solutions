c28f56c72f81fddafda65909e4d749136bec5148
Ashwin Rachha <43395687+AshwinRachha@users.noreply.github.com>
2022-04-12 16:18:48 -0400

    Time: 1108 ms (49.17%), Space: 31.9 MB (97.66%) - LeetHub

--- 870-advantage-shuffle/870-advantage-shuffle.py
+++ 870-advantage-shuffle/870-advantage-shuffle.py
@@ -1,22 +1,17 @@
+import bisect
 class Solution:
-    def binarySearch(self , arr , x):
-        low = 0 
-        high = len(arr)-1
-        while low <= high:
-            mid = (low+high)//2
-            if arr[mid] <= x:low = mid + 1
-            else: high = mid - 1
-        return low
-    
     def advantageCount(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        nums1.sort()
-        n = len(nums1)
+        
         ans = []
-        for i in nums2:
-            idx = self.binarySearch(nums1 , i)
-            if idx == n: idx = 0
+        n = len(nums1)
+        nums1.sort()
+        
+        for num in nums2:
+            
+            idx = bisect.bisect_right(nums1, num)
+            if idx == n:
+                idx = 0
             ans.append(nums1[idx])
             nums1.pop(idx)
             n-=1
         return ans
-        
