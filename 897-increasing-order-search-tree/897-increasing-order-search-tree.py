3f59f24c592923b47ea9bb4989010d43af2c032e
Ashwin Rachha <43395687+AshwinRachha@users.noreply.github.com>
2022-04-17 15:54:32 -0400

    Time: 38 ms (67.67%), Space: 13.9 MB (50.48%) - LeetHub

--- 897-increasing-order-search-tree/897-increasing-order-search-tree.py
+++ 897-increasing-order-search-tree/897-increasing-order-search-tree.py
@@ -5,24 +5,14 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    
-    def dfs(self, root, arr):
-        if root:
-            self.dfs(root.left, arr)
-            arr.append(root.val)
-            self.dfs(root.right, arr)
-        return arr
-    
-    def make_tree(self, arr, index):
-        node = None
-        if index < len(arr):
-            node = TreeNode(arr[index])
-            node.left = None
-            node.right = self.make_tree(arr, index + 1)
-        return node
     def increasingBST(self, root: TreeNode) -> TreeNode:
-        
-        arr = self.dfs(root, [])
-        return self.make_tree(arr, 0)
-    
-        
+        self.root = head = TreeNode(None)
+        def dfs(root):
+            if not root:
+                return None
+            dfs(root.left)
+            self.root.right = TreeNode(root.val)
+            self.root = self.root.right
+            dfs(root.right)
+        dfs(root)
+        return head.right
