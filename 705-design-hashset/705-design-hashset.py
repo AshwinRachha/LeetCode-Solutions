a5e97ea9f21b788e3b6da055c937e6b443b47ce7
Ashwin Rachha <43395687+AshwinRachha@users.noreply.github.com>
2022-04-21 18:18:18 -0400

    Time: 297 ms (52.09%), Space: 20.5 MB (27.36%) - LeetHub

--- 705-design-hashset/705-design-hashset.py
+++ 705-design-hashset/705-design-hashset.py
@@ -1,21 +1,57 @@
 class MyHashSet:
-
     def __init__(self):
-        
-        self.dic = defaultdict(bool)
+        self.hash_value = 768
+        self.hash_table = [Bucket() for i in range(self.hash_value)]
         
     def add(self, key: int) -> None:
+        idx = key % self.hash_value
+        self.hash_table[idx].insert(key)
         
-        self.dic[key] = True
-
     def remove(self, key: int) -> None:
+        idx = key % self.hash_value
+        self.hash_table[idx].delete(key)
         
-        self.dic[key] = False
-
     def contains(self, key: int) -> bool:
-        
-        return self.dic[key]
+        idx = key % self.hash_value
+        return self.hash_table[idx].exists(key)
 
+
+class Node:
+    def __init__(self, value, nextNode=None):
+        self.value = value
+        self.next = nextNode
+
+class Bucket:
+    def __init__(self):
+        # a pseudo head
+        self.head = Node(0)
+
+    def insert(self, newValue):
+        # if not existed, add the new element to the head.
+        if not self.exists(newValue):
+            newNode = Node(newValue, self.head.next)
+            # set the new head.
+            self.head.next = newNode
+
+    def delete(self, value):
+        prev = self.head
+        curr = self.head.next
+        while curr is not None:
+            if curr.value == value:
+                # remove the current node
+                prev.next = curr.next
+                return
+            prev = curr
+            curr = curr.next
+
+    def exists(self, value):
+        curr = self.head.next
+        while curr is not None:
+            if curr.value == value:
+                # value existed already, do nothing
+                return True
+            curr = curr.next
+        return False
 # Your MyHashSet object will be instantiated and called as such:
 # obj = MyHashSet()
 # obj.add(key)
