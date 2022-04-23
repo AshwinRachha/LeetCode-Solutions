46d336bca2039144c99bd0f4ee9d54a4a94c43af
Ashwin Rachha <43395687+AshwinRachha@users.noreply.github.com>
2022-04-23 10:40:45 -0400

    Time: 59 ms (34.86%), Space: 14.4 MB (27.41%) - LeetHub

---
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        def dfs(node, par = None):
+            if node:
+                node.par = par
+                dfs(node.left, node)
+                dfs(node.right, node)
+
+        dfs(root)
+        
+        q = deque([(target, 0)])
+        seen = {target}
+        while q:
+            if q[0][1] == k:
+                return [node.val for node, d in q]
+            node, d = q.popleft()
+            for nei in (node.left, node.right, node.par):
+                if nei and nei not in seen:
+                    seen.add(nei)
+                    q.append((nei,d+1))
+                    
+        return []
