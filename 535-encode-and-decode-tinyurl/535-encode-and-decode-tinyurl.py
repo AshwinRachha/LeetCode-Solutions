70964526c5771910a2f57bdde208abd80581dd65
Ashwin Rachha <43395687+AshwinRachha@users.noreply.github.com>
2022-04-23 12:36:52 -0400

    Time: 35 ms (86.51%), Space: 13.9 MB (69.91%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.py
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.py
@@ -1,18 +1,21 @@
 class Codec:
     def __init__(self):
         self.dic = dict()
+        self.counter = 0
     def encode(self, longUrl: str) -> str:
         """Encodes a URL to a shortened URL.
         """
-        if longUrl not in self.dic:
-            self.dic[longUrl] = longUrl
-        return self.dic[longUrl]
+        self.dic[self.counter] = longUrl
+        self.counter+=1
+        print(self.dic)
+        return 'http://tinyurl.com/' + str(self.counter-1)
+    
     def decode(self, shortUrl: str) -> str:
         """Decodes a shortened URL to its original URL.
         """
-        if shortUrl not in self.dic:
-            self.dic[shortUrl] = shortUrl
-        return self.dic[shortUrl]
+        print(self.dic, shortUrl)
+        index = shortUrl.split('/')[-1]
+        return self.dic[int(index)]
         
         
 
