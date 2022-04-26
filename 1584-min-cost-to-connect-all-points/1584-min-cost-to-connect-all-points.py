4a80e5d3159506b35e810ea04b1e94478d664479
Ashwin Rachha <43395687+AshwinRachha@users.noreply.github.com>
2022-04-26 19:25:34 -0400

    Time: 1595 ms (79.13%), Space: 78.6 MB (84.34%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        
+        n = len(points)
+        
+        # Min-heap to store minimum weight edge at top.
+        heap = [(0, 0)]
+        #Track Nodes which are included in MST
+        in_mst = [False] * n
+        mst_cost = 0
+        edges_used = 0
+        while edges_used < n:
+            weight, curr_node = heapq.heappop(heap)
+            if in_mst[curr_node]:
+                continue
+            in_mst[curr_node] = True
+            mst_cost += weight
+            edges_used += 1
+            
+            for next_node in range(n):
+                
+                if not in_mst[next_node]:
+                    next_weight = abs(points[curr_node][0] - points[next_node][0]) + \
+                                  abs(points[curr_node][1] - points[next_node][1])
+                    
+                    heapq.heappush(heap, (next_weight, next_node))
+            
+        return mst_cost
+            
+        
