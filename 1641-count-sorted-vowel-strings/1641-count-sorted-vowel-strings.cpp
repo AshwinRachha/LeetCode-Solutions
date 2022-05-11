bbbef8a17527dd8494e1a725fcd76227a9f4114e
Ashwin Rachha <43395687+AshwinRachha@users.noreply.github.com>
2022-05-11 08:36:02 -0400

    Time: 0 ms (100.00%), Space: 5.9 MB (58.60%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -1,27 +1,7 @@
 class Solution {
 public:
     
-    vector <char> vowels = {'a', 'e', 'i', 'o', 'u'};
-    
-    void backtrack(int n, char last, int &count)
-    {
-        if(n == 0)
-        {
-            count++;
-            return;
-        }
-        else
-        {
-            for(auto e : vowels)
-                if(last <= e)
-                    backtrack(n-1, e, count);
-        }
-    }
-    
-    int countVowelStrings(int n) 
-    {
-        int count = 0;
-        backtrack(n, ' ', count);
-        return count;
+    int countVowelStrings(int n) {
+        return (n + 4) * (n + 3) * (n + 2) * (n + 1) / 24;
     }
 };
