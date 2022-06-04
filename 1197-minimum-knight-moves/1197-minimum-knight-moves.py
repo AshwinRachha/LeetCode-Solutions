6b5057c547297794019c60703a9af8dcc4616046
Ashwin Rachha <43395687+AshwinRachha@users.noreply.github.com>
2022-06-04 12:43:09 -0400

    Time: 5848 ms (39.44%), Space: 76.3 MB (8.17%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.py
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.py
@@ -1,21 +1,26 @@
 class Solution:
     def minKnightMoves(self, x: int, y: int) -> int:
         
-        q = deque([(0,0)])
+        
+        min_moves = 0
+        directions = [(2, 1), (1, 2), (2, -1), (-1, 2), (-2, 1), (1, -2), (-2, -1), (-1, -2)]
+        q = deque([])
+        q.append((0, 0))
         seen = set()
         seen.add((0, 0))
-        min_steps = 0
-        nei = [(2, 1),(1,2), (-2, 1),(1, -2), (-1, 2),(2, -1), (-2, -1),(-1, -2)]
         while q:
-            for _ in range(len(q)):
-                node = q.popleft()
-                if node == (x, y):
-                    return min_steps
-                for x1, y1 in nei:
-                    new_x, new_y = node[0] + x1, node[1] + y1
+            size = len(q)
+            for _ in range(size):
+                x1, y1 = q.popleft()
+                if x1 == x and y1 == y:
+                    return min_moves
+                for x2, y2 in directions:
+                    new_x, new_y = x1 + x2, y1 + y2
                     if (new_x, new_y) not in seen:
-                        seen.add((new_x, new_y))
                         q.append((new_x, new_y))
-            min_steps += 1
-        return min_steps
+                        seen.add((new_x, new_y))
+            min_moves += 1
             
+            
+            
+        
