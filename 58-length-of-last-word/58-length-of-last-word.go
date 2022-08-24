cf8ad8b879cb4034416209f15d6478516626cd4e
Ashwin Rachha <43395687+AshwinRachha@users.noreply.github.com>
2022-08-24 16:22:36 -0400

    Time: 0 ms (100.00%), Space: 2.1 MB (37.21%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.go
@@ -0,0 +1,12 @@
+func lengthOfLastWord(s string) int {
+	len := len(s)
+	result := 0
+	for i := len - 1; i >= 0; i-- {
+		if s[i] != ' ' {
+			result += 1
+		} else if result > 0 {
+			return result
+		}
+	}
+	return result
+}
