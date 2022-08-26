b856a5a490b07e960ca5f6a5ce9db12ad3e05fcf
Ashwin Rachha <43395687+AshwinRachha@users.noreply.github.com>
2022-08-26 15:20:42 -0400

    Time: 365 ms (81.73%), Space: 16.4 MB (65.51%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,34 +1,26 @@
 class Solution:
-    
-    def __init__(self):
-        self.visited = set()
-        self.queue = deque()
-    
-    def bfs(self, grid, rows, columns, r, c):
-        
-        self.queue = deque([[r, c]])
-        while self.queue:
-            r,c = self.queue.popleft()
-            if (r, c) not in self.visited and grid[r][c] == '1':
-                self.visited.add((r,c))
-                
-                if r != 0 and grid[r-1][c] == '1':
-                    self.queue.append([r-1, c])
-                if c!= 0 and grid[r][c-1] == '1':
-                    self.queue.append([r, c-1])
-                if r!=rows-1 and grid[r+1][c] == '1':
-                    self.queue.append([r+1, c])
-                if c!=columns - 1 and grid[r][c+1] == '1':
-                    self.queue.append([r, c+1])
-        
     def numIslands(self, grid: List[List[str]]) -> int:
         
-        rows, columns = len(grid), len(grid[0])
-        count = 0
-        for r in range(rows):
-            for c in range(columns):
+        rows, cols = len(grid), len(grid[0])
+        
+        def dfs(r, c):
+            
+            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == "0":
+                return
+            grid[r][c] = "0"
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+        
+        ans = 0
+        
+        for i in range(rows):
+            for j in range(cols):
                 
-                if grid[r][c] == "1" and (r, c) not in self.visited:
-                    self.bfs(grid, rows, columns, r, c)
-                    count += 1
-        return count
+                if grid[i][j] == "1":
+                    ans += 1
+                    dfs(i,j)
+                
+        return ans
+        
