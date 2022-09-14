a8f033fcd4b6727fcd9da418a768e078687ac034
Ashwin Rachha <43395687+AshwinRachha@users.noreply.github.com>
2022-09-14 11:52:40 -0400

    Time: 99 ms (10.53%), Space: 16.3 MB (89.18%) - LeetHub

---
+++ 545-boundary-of-binary-tree/545-boundary-of-binary-tree.py
@@ -0,0 +1,53 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def boundaryOfBinaryTree(self, root: Optional[TreeNode]) -> List[int]:
+        
+        self.res = []
+        if not root:
+            return self.res
+        if not self.isLeaf(root):
+            self.res.append(root.val)
+        
+        t = root.left
+        while t:
+            if not self.isLeaf(t):
+                self.res.append(t.val)
+            if t.left:
+                t = t.left
+            else:
+                t = t.right
+        self.addLeaves(root)
+        
+        stack = []
+        
+        t = root.right
+        while t:
+            if not self.isLeaf(t):
+                stack.append(t.val)
+            if t.right:
+                t = t.right
+            else:
+                t = t.left
+        while stack:
+            self.res.append(stack.pop())
+        
+        return self.res
+        
+    def isLeaf(self, root):
+        return root.left == None and root.right == None
+    
+    def addLeaves(self, root):
+        if self.isLeaf(root):
+            self.res.append(root.val)
+        else:
+            if root.left:
+                self.addLeaves(root.left)
+            if root.right:
+                self.addLeaves(root.right)
+    
+    
