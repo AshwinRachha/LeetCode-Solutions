86504550154532c6d4788b5ed7f5ca9112d3c46d
Ashwin Rachha <43395687+AshwinRachha@users.noreply.github.com>
2022-09-16 16:16:31 -0400

    Time: 60 ms (52.33%), Space: 14 MB (77.53%) - LeetHub

--- 937-reorder-data-in-log-files/937-reorder-data-in-log-files.py
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.py
@@ -3,6 +3,6 @@ class Solution:
         
         letters = [l for l in logs if l.split()[1].isalpha()]
         digits = [d for d in logs if d.split()[1].isdigit()]
-        letters.sort(key=lambda x: (x.split(' ')[1:], x.split(' ')[0]))
+        letters.sort(key=lambda x: (x.split()[1:], x.split()[0]))
         return letters + digits
         
