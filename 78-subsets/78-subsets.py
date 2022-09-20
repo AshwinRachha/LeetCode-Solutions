a29063b11bc4edbd395ae0199cd9a64d95cedbba
Ashwin Rachha <43395687+AshwinRachha@users.noreply.github.com>
2022-09-20 18:18:46 -0400

    Time: 50 ms (63.74%), Space: 14.2 MB (35.81%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,15 +1,16 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         
-        output = []
-        
-        def backtrack(path, index, k):
-            if len(path) == k:
-                output.append(path[:])
-            for i in range(index, len(nums)):
+        outputs = []
+        n = len(nums)
+        def backtrack(index, k, path):
+            if len(path[:]) == k:
+                outputs.append(path[:])
+            for i in range(index, n):
                 path.append(nums[i])
-                backtrack(path, i + 1, k)
+                backtrack(i + 1, k, path)
                 path.pop()
-        for k in range(len(nums) + 1):
-            backtrack([], 0, k)
-        return output
+
+        for k in range(n+1):
+            backtrack(0, k, [])
+        return outputs
