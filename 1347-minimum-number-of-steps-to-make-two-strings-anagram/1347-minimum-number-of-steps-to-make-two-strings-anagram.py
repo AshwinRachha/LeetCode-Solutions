ada1b45e2f2fdd0a18e75065ab357952c8e96d23
Ashwin Rachha <43395687+AshwinRachha@users.noreply.github.com>
2022-09-21 16:01:19 -0400

    Time: 284 ms (60.07%), Space: 14.6 MB (26.71%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -3,7 +3,6 @@ class Solution:
         
         ans = 0
         c1, c2 = Counter(s), Counter(t)
-        print(c1, c2)
         for c in set(s + t):
             if c2[c] < c1[c]:
                 ans += c1[c] - c2[c]
