fe9725cb31a2ae6a31b01aba8ce3784be64685c3
Ashwin Rachha <43395687+AshwinRachha@users.noreply.github.com>
2022-09-24 18:28:04 -0400

    Time: 2775 ms (12.46%), Space: 18.4 MB (90.42%) - LeetHub

--- 1874-minimize-product-sum-of-two-arrays/1874-minimize-product-sum-of-two-arrays.py
+++ 1874-minimize-product-sum-of-two-arrays/1874-minimize-product-sum-of-two-arrays.py
@@ -1,11 +1,11 @@
-import heapq
 class Solution:
     def minProductSum(self, nums1: List[int], nums2: List[int]) -> int:
         
         nums1.sort()
-        prod = 0
-        nums2 = [-x for x in nums2]
-        heapq.heapify(nums2)
-        for num in nums1:
-            prod += heapq.heappop(nums2) * -1 * num
-        return prod
+        nums2.sort(reverse = True)
+        
+        ans = 0
+        for num1, num2 in zip(nums1, nums2):
+            ans += num1 * num2
+        return ans
+        
