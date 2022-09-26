5c6bf716aedf9a428e79a1bc9c02043ce7d891dd
Ashwin Rachha <43395687+AshwinRachha@users.noreply.github.com>
2022-09-26 17:22:17 -0400

    Time: 88 ms (36.33%), Space: 14.1 MB (34.67%) - LeetHub

---
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.py
@@ -0,0 +1,27 @@
+class Solution:
+    def equationsPossible(self, equations: List[str]) -> bool:
+        
+        graph = defaultdict(list)
+        for equation in equations:
+            if equation[1] == "=":
+                a, b = ord(equation[0]) - ord('a'), ord(equation[-1]) - ord('a')
+                graph[a].append(b)
+                graph[b].append(a)
+        
+        color = [-1] * 26
+        
+        def dfs(node, c):
+            if color[node] == -1:
+                color[node] = c
+                for nei in graph[node]:
+                    dfs(nei, c)
+        for i in range(26):
+            if color[i] == -1:
+                dfs(i, i)
+        for equation in equations:
+            if equation[1] == '!':
+                x = ord(equation[0]) - ord('a')
+                y = ord(equation[3]) - ord('a')
+                if color[x] == color[y]:
+                    return False
+        return True
