80fc9f816c7dcf12cd5289cff99e799722a49cda
Ashwin Rachha <43395687+AshwinRachha@users.noreply.github.com>
2022-09-28 15:57:43 -0400

    Time: 252 ms (6.51%), Space: 17.5 MB (19.26%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.py
+++ 261-graph-valid-tree/261-graph-valid-tree.py
@@ -1,23 +1,19 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        
         graph = defaultdict(list)
         for e1, e2 in edges:
             graph[e1].append(e2)
             graph[e2].append(e1)
-        seen = set()
+        visit = set()
         def dfs(node, parent):
-            if node in seen:
+            if node in visit:
                 return False
             else:
-                seen.add(node)
-                for child in graph[node]:
-                    if child == parent:
+                visit.add(node)
+                for nei in graph[node]:
+                    if nei == parent:
                         continue
-                    if not dfs(child, node):
+                    if not dfs(nei, node):
                         return False
                 return True
-            
-        return dfs(0, -1) and len(seen) == n
-
-        
+        return dfs(0, -1) and len(visit) == n
