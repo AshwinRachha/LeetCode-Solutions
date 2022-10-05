0c45ce5db3d6f6f0d36d8e23d7153ef56f3df22a
Ashwin Rachha <43395687+AshwinRachha@users.noreply.github.com>
2022-10-05 16:02:53 -0400

    Time: 89 ms (58.09%), Space: 13.9 MB (94.37%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.py
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.py
@@ -1,27 +1,22 @@
 class Solution:
     def equationsPossible(self, equations: List[str]) -> bool:
+        root = list(range(26))
+        def find(x):
+            if x != root[x]:
+                root[x] = find(root[x])
+            return root[x]
         
-        graph = defaultdict(list)
-        for equation in equations:
-            if equation[1] == "=":
-                a, b = ord(equation[0]) - ord('a'), ord(equation[-1]) - ord('a')
-                graph[a].append(b)
-                graph[b].append(a)
+        def union(x, y):
+            x, y = find(x), find(y)
+            root[x] = y
         
-        color = [-1] * 26
-        
-        def dfs(node, c):
-            if color[node] == -1:
-                color[node] = c
-                for nei in graph[node]:
-                    dfs(nei, c)
-        for i in range(26):
-            if color[i] == -1:
-                dfs(i, i)
-        for equation in equations:
-            if equation[1] == '!':
-                x = ord(equation[0]) - ord('a')
-                y = ord(equation[3]) - ord('a')
-                if color[x] == color[y]:
+        for eqn in equations:
+            if eqn[1] == '=':
+                x, y = ord(eqn[0])-ord('a'), ord(eqn[3])-ord('a')
+                union(x, y)
+        for eqn in equations:
+            if eqn[1] == '!':
+                x, y = ord(eqn[0])-ord('a'), ord(eqn[3])-ord('a')
+                if find(x) == find(y):
                     return False
         return True
