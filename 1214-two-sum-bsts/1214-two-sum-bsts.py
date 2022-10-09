76cad6d7a0d220f5fb14b667f3bd63942c9bba46
Ashwin Rachha <43395687+AshwinRachha@users.noreply.github.com>
2022-10-09 17:22:01 -0400

    Time: 178 ms (35.90%), Space: 17.1 MB (85.91%) - LeetHub

--- 1214-two-sum-bsts/1214-two-sum-bsts.py
+++ 1214-two-sum-bsts/1214-two-sum-bsts.py
@@ -7,24 +7,23 @@
 class Solution:
     def twoSumBSTs(self, root1: Optional[TreeNode], root2: Optional[TreeNode], target: int) -> bool:
         
-        dic = {}
-        def dfs1(root):
-            nonlocal dic
-            if root:
-                dic[root.val] = target - root.val
-                if root.left:
-                    dfs1(root.left)
-                if root.right:
-                    dfs1(root.right)
-        dfs1(root1)
+        stack, s = [], set()
+        while stack or root1:
+            while root1:
+                stack.append(root1)
+                root1 = root1.left
+            root1 = stack.pop()
+            s.add(target - root1.val)
+            root1 = root1.right
         
-        def dfs2(root):
-            if not root:
-                return False
-            else:
-                val = root.val
-                if target - val in dic:
-                    return True
-                return dfs2(root.left) or dfs2(root.right)
-        return dfs2(root2)
+        while stack or root2:
+            while root2:
+                stack.append(root2)
+                root2 = root2.left
+            root2 = stack.pop()
+            if root2.val in s:
+                return True
+            root2 = root2.right
+        
+        return False
         
