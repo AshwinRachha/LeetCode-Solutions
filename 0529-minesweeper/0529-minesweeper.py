2b36e080a2c391ec360ca4d219cf46540b5545cb
Ashwin Rachha <43395687+AshwinRachha@users.noreply.github.com>
2022-10-16 23:11:41 -0400

    Time: 228 ms (81.21%), Space: 17.2 MB (32.65%) - LeetHub

---
+++ 0529-minesweeper/0529-minesweeper.py
@@ -0,0 +1,29 @@
+class Solution:
+    
+    def getMines(self, board, x, y):
+        numMines = 0
+        for r in range(x-1, x + 2):
+            for c in range(y-1, y + 2):
+                if r >= 0 and r < len(board) and c >= 0 and c < len(board[0]) and board[r][c] == 'M':
+                    numMines += 1
+        return numMines
+    def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
+        
+        if not board:
+            return board
+        x, y = click
+        
+        if board[x][y] == 'M':
+            board[x][y] = 'X'
+        else:
+            numMines = self.getMines(board, x, y)
+            if numMines:
+                board[x][y] = str(numMines)
+            else:
+                board[x][y] = 'B'
+                for r in range(x-1, x + 2):
+                    for c in range(y-1, y + 2):
+                        if r >= 0 and r < len(board) and c >= 0 and c < len(board[0]) and board[r][c] != 'B':
+                            self.updateBoard(board, [r, c])
+
+        return board
