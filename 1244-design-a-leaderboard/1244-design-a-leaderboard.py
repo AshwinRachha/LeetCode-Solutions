56c553b431b0a56be87959b9924f4f6f6b85e717
Ashwin Rachha <43395687+AshwinRachha@users.noreply.github.com>
2022-10-24 17:05:14 -0400

    Time: 262 ms (7.26%), Space: 14.3 MB (71.15%) - LeetHub

--- 1244-design-a-leaderboard/1244-design-a-leaderboard.py
+++ 1244-design-a-leaderboard/1244-design-a-leaderboard.py
@@ -1,28 +1,28 @@
-import heapq
 class Leaderboard:
 
     def __init__(self):
         
-        self.leaderboard = defaultdict(int)
-        
+        self.leaderboard= defaultdict(int)
+
     def addScore(self, playerId: int, score: int) -> None:
         
         self.leaderboard[playerId] += score
 
     def top(self, K: int) -> int:
-        heap = []
+        
         res = 0
+        heap = []
         for num in self.leaderboard.values():
             heapq.heappush(heap, num)
             if len(heap) > K:
                 heapq.heappop(heap)
-        while heap:
+        while len(heap):
             res += heapq.heappop(heap)
         return res
-        
-
     def reset(self, playerId: int) -> None:
+        
         self.leaderboard[playerId] = 0
+
 # Your Leaderboard object will be instantiated and called as such:
 # obj = Leaderboard()
 # obj.addScore(playerId,score)
