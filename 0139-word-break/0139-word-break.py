607f88e74f38d51b862647e8cff513705f8a2e3b
Ashwin Rachha <43395687+AshwinRachha@users.noreply.github.com>
2022-10-25 16:25:23 -0400

    Time: 37 ms (96.80%), Space: 14.2 MB (14.13%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,15 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        
+        
+        wordDict = set(wordDict)
+        n = len(s)
+        @lru_cache(maxsize = None)
+        def backtrack(index):
+            if index == n:
+                return True
+            for i in range(index + 1, n + 1):
+                if s[index:i] in wordDict and backtrack(i):
+                    return True
+            return False
+        return backtrack(0)
