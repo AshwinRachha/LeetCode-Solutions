f92a8e528c24106d1233c4b17e48024e35b8a4a0
Ashwin Rachha <43395687+AshwinRachha@users.noreply.github.com>
2022-10-26 17:18:12 -0400

    Time: 1300 ms (10.19%), Space: 27.2 MB (65.14%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
@@ -3,14 +3,21 @@ class Solution:
         """
         1 2 3 4 5 6 1 -> s = 22, n = 7, k = 3
         """
-        totalSum = sum(cardPoints)
-        n = len(cardPoints)
-        if k == n:
+        
+        totalSum, n = sum(cardPoints), len(cardPoints)
+        # We have to take all cards so return sum of array
+        if n == k:
             return totalSum
+        # else, we have a choice to include either 1st element, 1st and 2nd element,
+        # 1...k elements from the front, or we can take the last element, to n - k from the end
+        # or we can take k elements from the beginning and the end. 
+        
+        # For this purpose we need a sliding window across the array and we will pick out 
+        # the max out of them
         currentWindowSum = sum(cardPoints[:n-k])
         ans = currentWindowSum
         for i in range(n-k, n):
-            currentWindowSum -= cardPoints[i-(n-k)]
+            currentWindowSum -= cardPoints[i - (n - k)]
             currentWindowSum += cardPoints[i]
             ans = min(ans, currentWindowSum)
         return totalSum - ans
