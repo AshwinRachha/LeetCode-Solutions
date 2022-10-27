037c5d469036375e3773e68c614522504e0ba0f5
Ashwin Rachha <43395687+AshwinRachha@users.noreply.github.com>
2022-10-27 15:09:39 -0400

    Time: 55 ms (76.91%), Space: 14 MB (35.19%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.py
@@ -0,0 +1,17 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        
+        """
+        
+        1. Use library functions to convert to integer, compute sum, convert to string
+        
+        2. Design Custom Function to do conversions mentioned above.
+        
+        3. Iterate through the strings and then add using elementary math
+        
+        """
+        
+        return str(int(num1) + int(num2))
+        
+        
+        
