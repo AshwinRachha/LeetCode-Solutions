527ed75dff7bc81755a0453349d595d3fc1523eb
Ashwin Rachha <43395687+AshwinRachha@users.noreply.github.com>
2022-10-27 15:16:16 -0400

    Time: 92 ms (29.73%), Space: 14 MB (60.06%) - LeetHub

--- 0415-add-strings/0415-add-strings.py
+++ 0415-add-strings/0415-add-strings.py
@@ -5,13 +5,42 @@ class Solution:
         
         1. Use library functions to convert to integer, compute sum, convert to string
         
-        2. Design Custom Function to do conversions mentioned above.
+        return str(int(num1) + int(num2))
         
+        2. Design Custom Function to do conversions mentioned above.
+                
         3. Iterate through the strings and then add using elementary math
+        
         
         """
         
-        return str(int(num1) + int(num2))
+        s, c = 0, 0
+        
+        n, m = len(num1) - 1, len(num2) - 1
+        
+        ans = ''
+        
+        while n >= 0 or m >= 0:
+            
+            if n >= 0:
+                val1 = ord(num1[n]) - ord('0')
+            else:
+                val1 = 0
+            
+            if m >= 0:
+                val2 = ord(num2[m]) - ord('0')
+            else:
+                val2 = 0
+            #print(val1, val2)
+            
+            c, s = divmod(val1 + val2 + c, 10)
+            ans += str(s)
+            n -= 1
+            m -= 1
+        if c:
+            ans += '1'
+        return ans[::-1]
+        
         
         
         
