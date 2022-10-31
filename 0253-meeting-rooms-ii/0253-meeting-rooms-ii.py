70fd8bbdcedea1f7edadcdb1e568f414d425d3ba
Ashwin Rachha <43395687+AshwinRachha@users.noreply.github.com>
2022-10-31 07:01:21 -0400

    Time: 180 ms (38.44%), Space: 17.4 MB (83.66%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,6 +1,6 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        
+        """
         intervals.sort()
         heap = []
         for start, end in intervals:
@@ -9,4 +9,17 @@ class Solution:
             else:
                 heapq.heappushpop(heap, end)
         return len(heap)
-    
+        """
+        used_rooms = 0
+        n = len(intervals)
+        start_timings = sorted(interval[0] for interval in intervals)
+        end_timings = sorted(interval[1] for interval in intervals)
+        start, end = 0, 0
+        while start < n:
+            if start_timings[start] >= end_timings[end]:
+                used_rooms -= 1
+                end += 1
+            used_rooms += 1
+            start += 1
+        return used_rooms
+            
